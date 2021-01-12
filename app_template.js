b362b75b1c9b0b27ff7859c4b3b97b216c6d4170
arclxngth <59277471+arclxngth@users.noreply.github.com>
2021-01-12 13:57:08 +0700

    1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,24 @@
 //your code here!
+
+const modify = document.getElementById("button-compute")
+modify.addEventListener("click", function() {
+    let element = document.createElement("p")
+
+    const input = document.getElementById("input-number")
+
+    let list = document.getElementById("compute-result")
+
+    element = +input.value - 5;
+
+    list.append(element)
+})
+
+// -------------------------------------------------
+
+const table = document.getElementById("table")
+table.addEventListener("click", function(){
+    const row = document.getElementById("input-row")
+    const clm = document.getElementById("input-col")
+
+    
+})
